91614a30b6c2fb94f2fd6ac57968a258e38e341f
Rui En <neoruien123@gmail.com>
2021-05-27 15:48:53 +0800

    Change text in Home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,13 +6,10 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hi, I am 
-                    <span> Lorem Ipsum.</span>
+                    <span> Rui En.</span>
                 </h1>
                 <p className="h-sub-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia 
-                    quibusdam. Blanditiis doloremque nemo ex facilis neque. 
-                    A sint ipsam earum nemo omnis et.
+                    I am a website and mobile application developer.
                 </p>
             </header>
             <button className="btn">View my work</button>
