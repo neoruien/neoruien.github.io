0090326e972e8d60965d01a70dbca23248b7761f
Rui En <neoruien123@gmail.com>
2021-06-08 10:51:49 +0800

    Revert url change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="my-website">
       <App /> 
     </BrowserRouter>
   </React.StrictMode>,
