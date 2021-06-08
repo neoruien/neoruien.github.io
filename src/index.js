b2854bc930f536ab6018bec6f03c8762a6679204
Rui En <neoruien123@gmail.com>
2021-06-08 10:32:54 +0800

    Modify url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-website">
+    <BrowserRouter>
       <App /> 
     </BrowserRouter>
   </React.StrictMode>,
