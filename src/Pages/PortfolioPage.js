5efe4cc3681d8d7ac6a1157f9274f3d5e4a3ed2f
Rui En <neoruien123@gmail.com>
2021-06-09 13:00:53 +0800

    Refactor PortfolioPage code

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -2,32 +2,30 @@ import React, { useState } from 'react';
 import PortfolioFilter from '../components/PortfolioFilter';
 import PortfolioMenu from '../components/PortfolioMenu';
 import Title from '../components/Title';
-import portfolios from '../data/allportfolios';
+import allPortfolios from '../data/allportfolios';
 
-const allCategories = ['All', ...new Set(portfolios.map(item => item.category))];
+const allCategories = ['All', ...new Set(allPortfolios.map(item => item.category))];
 
 function PortfolioPage() {
     const [categories, setCategories] = useState(allCategories);
-    const [menuItems, setMenuItems] = useState(portfolios);
+    const [menuItems, setMenuItems] = useState(allPortfolios);
 
     const filter = (category) =>{
         if (category === 'All'){
-            setMenuItems(portfolios)
+            setMenuItems(allPortfolios)
             return;
         }
-        const filteredData = portfolios.filter((item) => {
+        const filteredData = allPortfolios.filter((item) => {
             return item.category === category;
         })
         setMenuItems(filteredData);
     }
 
     return (
-        <div className="PortfolioPage">
+        <div>
             <Title title={'My Portfolio'} />
-            <div className="portfolios-data">
-                <PortfolioFilter filter={filter} categories={categories} />
-                <PortfolioMenu menuItem={menuItems} />
-            </div>
+            <PortfolioFilter filter={filter} categories={categories} />
+            <PortfolioMenu menuItem={menuItems} />
         </div>
     )
 }
