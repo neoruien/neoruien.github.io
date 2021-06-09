d01ed69ba3e55a483a54022dff5210855b9e0d3a
Rui En <neoruien123@gmail.com>
2021-06-09 12:07:01 +0800

    Add carousel

--- src/Components/SkillSection.js
+++ src/Components/SkillSection.js
@@ -1,30 +1,12 @@
 import React from 'react'
 
-function SkillSection({title,progress}) {
+function SkillSection({image,title}) {
     return (
         <div className="SkillSection">
-            <div className="skills-container">
-                <h5 className="skill-title">{title}</h5>
-                <div className="skill-bar">
-                    <p className="skill-text">{progress}</p>
-                    <div className="skill-progress">
-                        <div className="progress">
-                            <div className="inner-progress" style={{width:progress}}></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <img src={image} alt="" width="80rem"/>
+            <p>{title}</p>
         </div>
     )
 }
-
-// function SkillSection({image,title}) {
-//     return (
-//         <div className="SkillSection">
-//             <img src={image} alt="" width="80rem"/>
-//             <p>{title}</p>
-//         </div>
-//     )
-// }
 
 export default SkillSection;
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
 import IntroSection from '../components/IntroSection';
 import SkillSection from '../components/SkillSection';
 import InterestSection from '../components/InterestSection';
-import softwareDev from '../img/about/software_development.svg';
-import machineLearning from '../img/about/machine_learning.svg';
-import dataAnalytics from '../img/about/data_analytics.svg';
+
+import Carousel from "react-simply-carousel";
+import {faAngleLeft} from '@fortawesome/free-solid-svg-icons';
+import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import java from '../img/about/java.svg';
 import python from '../img/about/python.svg';
 import html from '../img/about/html.svg';
@@ -20,60 +23,105 @@ import adobexd from '../img/about/adobexd.svg';
 import tableau from '../img/about/tableau.svg';
 import unity from '../img/about/unity.svg';
 
+import softwareDev from '../img/about/software_development.svg';
+import machineLearning from '../img/about/machine_learning.svg';
+import dataAnalytics from '../img/about/data_analytics.svg';
+
 function AboutPage() {
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
     return (
         <div className="AboutPage">
             <Title title={'About Me'} />
             <IntroSection />
-            
             <Title title={'My Skills'} />
-            <div className="skills-list">
-                <SkillSection title={"React JS"} progress={"80%"} />
-                <SkillSection title={"Java"} progress={"80%"} />
-                <SkillSection title={"Javascript"} progress={"80%"} />
-                <SkillSection title={"Adobe XD"} progress={"80%"} />
-                <SkillSection title={"Python"} progress={"70%"} />
-                <SkillSection title={"PostgreSQL"} progress={"70%"} />
-                <SkillSection title={"R"} progress={"60%"} />
-                <SkillSection title={"Flutter"} progress={"60%"} />
-                <SkillSection title={"MATLAB"} progress={"60%"} />
-                <SkillSection title={"React Native"} progress={"50%"} />
-                <SkillSection title={"Unity"} progress={"50%"} />
-                <SkillSection title={"Tableau"} progress={"50%"} />
+            <div>
+                <Carousel
+                    activeSlideIndex={activeSlideIndex}
+                    onRequestChange={setActiveSlideIndex}
+                    itemsToShow={1}
+                    itemsToScroll={1}
+                    updateOnItemClick
+                    containerProps={{
+                        style: {
+                            width: "100%",
+                            justifyContent: "space-between"
+                        }
+                    }}
+                    forwardBtnProps={{
+                        children: <FontAwesomeIcon icon={faAngleRight} className="carousel-button"/>,
+                        style: {
+                            width: "2.5rem",
+                            height: "2.5rem",
+                            alignSelf: "center",
+                            background: "none",
+                            border: "none",
+                            cursor: "pointer",
+                        }
+                    }}
+                    backwardBtnProps={{
+                        children: <FontAwesomeIcon icon={faAngleLeft} className="carousel-button"/>,
+                        style: {
+                            width: "2.5rem",
+                            height: "2.5rem",
+                            alignSelf: "center",
+                            background: "none",
+                            border: "none",
+                            cursor: "pointer",
+                        }
+                    }}
+                    speed={500}
+                >
+                    <div className="skills-category">
+                        <p>Programming languages:</p>
+                        <div className="skills-container">
+                            <SkillSection image={java} title="Java"/>
+                            <SkillSection image={python} title="Python"/>
+                            <SkillSection image={html} title="HTML"/>
+                            <SkillSection image={css} title="CSS"/>
+                            <SkillSection image={javascript} title="Javascript"/>
+                            <SkillSection image={r} title="R"/>
+                            <SkillSection image={matlab} title="MATLAB"/>
+                        </div>
+                    </div>
+                    <div className="skills-category">
+                        <p>Frameworks:</p>
+                        <div className="skills-container">
+                            <SkillSection image={react} title="React"/>
+                            <SkillSection image={flutter} title="Flutter"/>
+                        </div>
+                    </div>
+                    <div className="skills-category">
+                        <p>Database systems:</p>
+                        <div className="skills-container">
+                            <SkillSection image={postgresql} title="PostgreSQL"/>
+                        </div>
+                    </div>
+                    <div className="skills-category">
+                        <p>Prototyping tools:</p>
+                        <div className="skills-container">
+                            <SkillSection image={adobexd} title="Adobe XD"/>
+                        </div>
+                    </div>
+                    <div className="skills-category">
+                        <p>Statistical tools:</p>
+                        <div className="skills-container">
+                            <SkillSection image={tableau} title="Tableau"/>
+                        </div>
+                    </div>
+                    <div className="skills-category">
+                        <p>Game engines:</p>
+                        <div className="skills-container">
+                            <SkillSection image={unity} title="Unity"/>
+                        </div>
+                    </div>
+                </Carousel>
             </div>
-            
-            {/* <p>Programming languages:</p>
-            <SkillSection image={java} title="Java"/>
-            <SkillSection image={python} title="Python"/>
-            <SkillSection image={html} title="HTML"/>
-            <SkillSection image={css} title="CSS"/>
-            <SkillSection image={javascript} title="Javascript"/>
-            <SkillSection image={r} title="R"/>
-            <SkillSection image={matlab} title="MATLAB"/>
-
-            <p>Frameworks:</p>
-            <SkillSection image={react} title="React"/>
-            <SkillSection image={flutter} title="Flutter"/>
-
-            <p>Database systems:</p>
-            <SkillSection image={postgresql} title="PostgreSQL"/>
-
-            <p>Prototyping tools:</p>
-            <SkillSection image={adobexd} title="Adobe XD"/>
-
-            <p>Statistical tools:</p>
-            <SkillSection image={tableau} title="Tableau"/>
-
-            <p>Game engines:</p>
-            <SkillSection image={unity} title="Unity"/> */}
-
             <Title title={'My Interests'} />
             <div className="interests-list">
                 <InterestSection image={softwareDev} title={'Software Dev'} />
                 <InterestSection image={machineLearning} title={'Machine Learning'} />
                 <InterestSection image={dataAnalytics} title={'Data Analytics'} />                
             </div>
-
         </div>
     )
 }
