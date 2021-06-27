d92bc580e83b43c44770037b364272732e1bfbb2
Rui En <neoruien123@gmail.com>
2021-06-27 21:15:28 +0800

    Rename folders

--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import Title from '../components/Title';
-import allBlogs from '../data/allBlogs';
+import React from "react";
+import Title from "../components/Title";
+import allBlogs from "../data/allBlogs";
 
 function BlogPage() {
-    return (
-        <div >
-            <div className="b-title">
-                <Title title={'Recent Blogs'} span={'Recent Blogs'} />
+  return (
+    <div>
+      <div className="b-title">
+        <Title title={"Recent Blogs"} span={"Recent Blogs"} />
+      </div>
+      <div className="BlogPage">
+        {allBlogs.map((blog) => {
+          return (
+            <div className="blog" key={blog.id}>
+              <div className="blog-content">
+                <img src={blog.image} alt="" />
+                <a href={blog.link} className="blog-link">
+                  {blog.title}
+                </a>
+              </div>
             </div>
-            <div className="BlogPage">
-                {
-                    allBlogs.map((blog)=>{
-                        return <div className="blog" key={blog.id}>
-                            <div className="blog-content">
-                                <img src={blog.image} alt=""/>
-                                <a href={blog.link} className="blog-link">
-                                    {blog.title}
-                                </a>
-                            </div>
-                        </div>
-                    })
-                }
-            </div>
-            
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default BlogPage;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,28 +1,24 @@
-import React from 'react';
+import React from "react";
 import ParticlesBackground from "../components/ParticlesBackground";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
-    return (
-        <div>
-            <ParticlesBackground />
-            <div className="HomePage">
-                <h1 className="home-text">
-                    Hi, I am 
-                    <span> Rui En.</span>
-                </h1>
-                <p className="home-subtext">
-                    I develop website applications.
-                </p>
-            
-                <NavLink to="/portfolio" exact activeClassName="active">
-                    <button className="my-button">
-                        View my work
-                    </button>
-                </NavLink>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <ParticlesBackground />
+      <div className="HomePage">
+        <h1 className="home-text">
+          Hi, I am
+          <span> Rui En.</span>
+        </h1>
+        <p className="home-subtext">I develop website applications.</p>
+
+        <NavLink to="/portfolio" exact activeClassName="active">
+          <button className="my-button">View my work</button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
