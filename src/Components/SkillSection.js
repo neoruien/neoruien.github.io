92ee60f93a3f7714696eb9e234f5baafea0babee
Rui En <neoruien@u.nus.edu>
2022-03-22 06:42:16 -0400

    Update skill section

--- src/Components/SkillSection.js
+++ src/Components/SkillSection.js
@@ -1,118 +1,126 @@
-import React, { useState } from 'react';
-import SkillCard from './SkillCard';
+import React, { useState } from "react";
+import SkillCard from "./SkillCard";
 
 import Carousel from "react-simply-carousel";
-import {faAngleLeft} from '@fortawesome/free-solid-svg-icons';
-import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import java from '../img/about/java.svg';
-import python from '../img/about/python.svg';
-import html from '../img/about/html.svg';
-import css from '../img/about/css.svg';
-import javascript from '../img/about/javascript.svg';
-import r from '../img/about/r.svg';
+import java from "../img/about/java.svg";
+import python from "../img/about/python.svg";
+import html from "../img/about/html.svg";
+import css from "../img/about/css.svg";
+import javascript from "../img/about/javascript.svg";
+import r from "../img/about/r.svg";
 // import matlab from '../img/about/matlab.svg';
-import react from '../img/about/react.svg';
-import flutter from '../img/about/flutter.svg';
-import postgresql from '../img/about/postgresql.svg';
-import adobexd from '../img/about/adobexd.svg';
-import tableau from '../img/about/tableau.svg';
-import unity from '../img/about/unity.svg';
+import react from "../img/about/react.svg";
+import flutter from "../img/about/flutter.svg";
+import postgresql from "../img/about/postgresql.svg";
+import adobexd from "../img/about/adobexd.svg";
+import figma from "../img/about/figma.svg";
+import balsamiq from "../img/about/balsamiq.svg";
+import tableau from "../img/about/tableau.svg";
+import unity from "../img/about/unity.svg";
 
 function SkillSection() {
-    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-    return (
-        <div className="SkillSection">
-            <Carousel
-                activeSlideIndex={activeSlideIndex}
-                onRequestChange={setActiveSlideIndex}
-                itemsToShow={1}
-                itemsToScroll={1}
-                updateOnItemClick
-                containerProps={{
-                    style: {
-                        width: "100%",
-                        margin: "0",
-                        padding: "0",
-                    }
-                }}
-                forwardBtnProps={{
-                    children: <FontAwesomeIcon icon={faAngleRight} className="carousel-button"/>,
-                    style: {
-                        width: "2.5rem",
-                        height: "2.5rem",
-                        alignSelf: "center",
-                        background: "none",
-                        border: "none",
-                        cursor: "pointer",
-                        margin: "0",
-                        marginLeft: ".5rem",
-                        position: "relative",
-                    }
-                }}
-                backwardBtnProps={{
-                    children: <FontAwesomeIcon icon={faAngleLeft} className="carousel-button"/>,
-                    style: {
-                        width: "2.5rem",
-                        height: "2.5rem",
-                        alignSelf: "center",
-                        background: "none",
-                        border: "none",
-                        cursor: "pointer",
-                        margin: "0",
-                        marginRight: ".5rem",
-                        position: "relative",
-                    }
-                }}
-                speed={500}
-            >
-                <div className="skill-category">
-                    <p className="category-title">Programming languages:</p>
-                    <div className="category-cards">
-                        <SkillCard image={java} title="Java"/>
-                        <SkillCard image={python} title="Python"/>
-                        <SkillCard image={html} title="HTML"/>
-                        <SkillCard image={css} title="CSS"/>
-                        <SkillCard image={javascript} title="Javascript"/>
-                        <SkillCard image={r} title="R"/>
-                        {/* <SkillCard image={matlab} title="MATLAB"/> */}
-                    </div>
-                </div>
-                <div className="skill-category">
-                    <p className="category-title">Frameworks:</p>
-                    <div className="category-cards">
-                        <SkillCard image={react} title="React"/>
-                        <SkillCard image={flutter} title="Flutter"/>
-                    </div>
-                </div>
-                <div className="skill-category">
-                    <p className="category-title">Database systems:</p>
-                    <div className="category-cards">
-                        <SkillCard image={postgresql} title="PostgreSQL"/>
-                    </div>
-                </div>
-                <div className="skill-category">
-                    <p className="category-title">Prototyping tools:</p>
-                    <div className="category-cards">
-                        <SkillCard image={adobexd} title="Adobe XD"/>
-                    </div>
-                </div>
-                <div className="skill-category">
-                    <p className="category-title">Statistical tools:</p>
-                    <div className="category-cards">
-                        <SkillCard image={tableau} title="Tableau"/>
-                    </div>
-                </div>
-                <div className="skill-category">
-                    <p className="category-title">Game engines:</p>
-                    <div className="category-cards">
-                        <SkillCard image={unity} title="Unity"/>
-                    </div>
-                </div>
-            </Carousel>
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  return (
+    <div className="SkillSection">
+      <Carousel
+        activeSlideIndex={activeSlideIndex}
+        onRequestChange={setActiveSlideIndex}
+        itemsToShow={1}
+        itemsToScroll={1}
+        updateOnItemClick
+        containerProps={{
+          style: {
+            width: "100%",
+            margin: "0",
+            padding: "0",
+          },
+        }}
+        forwardBtnProps={{
+          children: (
+            <FontAwesomeIcon icon={faAngleRight} className="carousel-button" />
+          ),
+          style: {
+            width: "2.5rem",
+            height: "2.5rem",
+            alignSelf: "center",
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+            margin: "0",
+            marginLeft: ".5rem",
+            position: "relative",
+          },
+        }}
+        backwardBtnProps={{
+          children: (
+            <FontAwesomeIcon icon={faAngleLeft} className="carousel-button" />
+          ),
+          style: {
+            width: "2.5rem",
+            height: "2.5rem",
+            alignSelf: "center",
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+            margin: "0",
+            marginRight: ".5rem",
+            position: "relative",
+          },
+        }}
+        speed={500}
+      >
+        <div className="skill-category">
+          <p className="category-title">Programming languages:</p>
+          <div className="category-cards">
+            <SkillCard image={java} title="Java" />
+            <SkillCard image={python} title="Python" />
+            <SkillCard image={html} title="HTML" />
+            <SkillCard image={css} title="CSS" />
+            <SkillCard image={javascript} title="Javascript" />
+            <SkillCard image={r} title="R" />
+            {/* <SkillCard image={matlab} title="MATLAB"/> */}
+          </div>
         </div>
-    )
+        <div className="skill-category">
+          <p className="category-title">Frameworks:</p>
+          <div className="category-cards">
+            <SkillCard image={react} title="React" />
+            <SkillCard image={flutter} title="Flutter" />
+          </div>
+        </div>
+        <div className="skill-category">
+          <p className="category-title">Database systems:</p>
+          <div className="category-cards">
+            <SkillCard image={postgresql} title="PostgreSQL" />
+          </div>
+        </div>
+        <div className="skill-category">
+          <p className="category-title">Prototyping tools:</p>
+          <div className="category-cards">
+            <SkillCard image={adobexd} title="Adobe XD" />
+            <SkillCard image={figma} title="Figma" />
+            <SkillCard image={balsamiq} title="Balsamiq" />
+          </div>
+        </div>
+        <div className="skill-category">
+          <p className="category-title">Statistical tools:</p>
+          <div className="category-cards">
+            <SkillCard image={tableau} title="Tableau" />
+          </div>
+        </div>
+        <div className="skill-category">
+          <p className="category-title">Game engines:</p>
+          <div className="category-cards">
+            <SkillCard image={unity} title="Unity" />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default SkillSection;
